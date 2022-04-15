74890aec4d71f7d98ec57751240cf6cf2705b5f0
Lincoln William <Lincoln_27@hotmail.com>
2022-04-15 10:50:59 -0300

    Calculando a área do Retangulo.

---
+++ Ativ06.py
@@ -0,0 +1,9 @@
+# calculando a área do retangulo
+base = float (input('Digite a Base do retângulo:'))
+altura = float (input('Digite a Altura do retângulo:'))
+area = base * altura
+
+print (f'A área do Retangulo é: {area}')
+
+# uma outra opção de print - retirando a linha da -> area = base * altura
+# print('A área do Retangulo é: ', base * altura)
