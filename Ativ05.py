6081e0faf085f4c0b5281d44397ff6db42854840
Lincoln William <Lincoln_27@hotmail.com>
2022-04-15 10:27:58 -0300

    # Transformar uma temperatura de Fahrenheit para Celsius.

---
+++ Ativ05.py
@@ -0,0 +1,6 @@
+# Transformar uma temperatura de Fahrenheit para Celsius.
+
+tf =  int(input("Digite a temperatura °F: "))
+tc = (tf - 32 ) * 5 /9
+
+print(f"A temperatura de {tf}°F corresponde a {tc}°C")
