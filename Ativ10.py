4c850ee0c02fd8fef84342d77dfd7248e6ca9f18
Lincoln William <Lincoln_27@hotmail.com>
2022-04-17 20:07:46 -0300

    informe se o número é positivo ou negativo

---
+++ Ativ10.py
@@ -0,0 +1,6 @@
+# Solicite um número ao usuário e mostre se o número é positivo ou negativo.
+numero = int(input('Digite um número: '))
+if numero >= 0:
+    print ('O número é positivo!')
+else:
+    print('O número é negativo!')
